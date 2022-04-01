831d6a217245a0052bcd57eec0cc902984615af9
AkinuM <avia65461321@gmail.com>
2022-04-02 00:09:37 +0300

    fix lab1

--- lab1.py
+++ lab1.py
@@ -1,1 +1,8 @@
-text = input("Enter text: ")
+from functions import *
+
+if __name__ == "__main__":
+    text = input("Enter text: ")
+    n = int(input("Enter n: "))
+    k = int(input("Enter k: "))
+    
+    solve(text, n, k)
